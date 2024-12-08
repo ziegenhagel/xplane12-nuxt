ffa48c2e20ed9a48163927ecc8415c5d937c2ee7
Dominik <dominik.ziegenhagel@gmail.com>
2024-12-09 00:04:21 +0100

    reduce places for ip

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ yarn
 # starten
 
 1. start xplane
-2. write your ip into `server/utils/constants.ts` and `composables/useXPlane.ts`
+2. write your ip into `server/utils/constants.ts`
 2. `lcp --proxyUrl http://localhost:8086 --port 8087`
 2. `yarn dev --host`
 3. call `http://[your-ip]:3000` in ipad
--- composables/useXPlane.ts
+++ composables/useXPlane.ts
@@ -1,10 +1,9 @@
-import {XPLANE_API_BASE} from "~/server/utils/constants";
+import {XPLANE_API_BASE, NUXT_API_BASE} from "~/server/utils/constants";
 
 export const useXPlane = () => {
 
-    const url = 'http://192.168.178.35:3000'
     const readDataref = async (name) => {
-        return await fetch(url+'/api/xplane/read', {
+        return await fetch(NUXT_API_BASE+'/api/xplane/read', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
@@ -15,7 +14,7 @@ export const useXPlane = () => {
     }
 
     const writeDataref = async (name, value) => {
-        return await fetch(url+'/api/xplane/write', {
+        return await fetch(NUXT_API_BASE+'/api/xplane/write', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
