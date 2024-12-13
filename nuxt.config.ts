b49a9ca519e3652c0d5e7de27707bca406fd952f
Dominik <dominik.ziegenhagel@gmail.com>
2024-12-13 12:23:51 +0100

    web app capable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon']
+  devtools: { enabled: false },
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon'],
+  app: {
+    head: {
+      meta: [
+        { name: 'mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'default' },
+        { name: 'apple-mobile-web-app-title', content: 'Your App Name' },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'
+        }
+      ],
+      link: [
+        { rel: 'manifest', href: '/manifest.json' },
+        { rel: 'apple-touch-icon', href: '/icon-512x512.png' }
+      ]
+    }
+  }
 })
